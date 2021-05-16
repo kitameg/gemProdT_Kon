5a42b1ba711324f57f5814580412d14ba4b9b2b6
kitameg <cloud@spilikin.dev>
2021-05-16 11:28:57 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // project version
-version = "4.8.2"
+version = "4-wip"
+val git by extra("https://github.com/kitameg/${project.name}.git")
 
 repositories {
     mavenCentral()
@@ -11,7 +12,71 @@ plugins {
 
 tasks.named<org.asciidoctor.gradle.jvm.AsciidoctorTask>("asciidoctor") {
     baseDirFollowsSourceDir()
-    setOutputDir("${buildDir}/asciidoc/${project.version}")
 }
 
 tasks.named("build") { dependsOn("asciidoctor") }
+
+task<org.asciidoctor.gradle.jvm.AsciidoctorTask>("ghpagesCreateRoot") {
+    setSourceDir(file("."))
+    sources(delegateClosureOf<PatternSet> {
+      include("README.adoc")
+    })
+    setOutputDir(layout.buildDirectory) 
+}
+
+task<Exec>("ghpagesClone") {
+    delete("${buildDir}/ghpages")
+    mkdir("${buildDir}/ghpages")
+    workingDir("${buildDir}/ghpages")
+    commandLine("git", "clone", git, "-b", "ghpages")
+}
+
+task<Copy>("ghpagesCopyAsciidoc") {
+    dependsOn("build")
+    from(layout.buildDirectory.dir("docs/asciidoc"))
+    into(layout.buildDirectory.dir("ghpages/${project.name}/${project.version}"))
+}
+
+task("publish") {
+    dependsOn("ghpagesCopyAsciidoc", "ghpagesClone")
+    doLast {
+        exec {
+            workingDir("${buildDir}/ghpages/${project.name}")
+            commandLine("git", "add", "*")
+        } 
+        exec {
+            workingDir("${buildDir}/ghpages/${project.name}")
+            commandLine("git", "commit", "-m", "Publish version ${project.version}", "--amend")
+        } 
+        exec {
+            workingDir("${buildDir}/ghpages/${project.name}")
+            commandLine("git", "push", "-f", "-u", "origin", "ghpages")
+        } 
+    }
+
+}
+
+task<Copy>("ghpagesCopyRoot") {
+    dependsOn("ghpagesCreateRoot", "ghpagesClone")
+    from(layout.buildDirectory.file("${buildDir}/README.html"))
+    into(layout.buildDirectory.file("ghpages/${project.name}"))
+    rename("README.html", "index.html")
+}
+
+task("publishRoot") {
+    dependsOn("ghpagesCopyRoot")
+    doLast {
+        exec {
+            workingDir("${buildDir}/ghpages/${project.name}")
+            commandLine("git", "add", "index.html")
+        } 
+        exec {
+            workingDir("${buildDir}/ghpages/${project.name}")
+            commandLine("git", "commit", "-m", "Publish index.html", "--amend")
+        } 
+        exec {
+            workingDir("${buildDir}/ghpages/${project.name}")
+            commandLine("git", "push", "-f", "-u", "origin", "ghpages")
+        } 
+    }
+}
